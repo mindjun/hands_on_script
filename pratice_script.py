5a15c7c07fb7b6c558166d018db0d2870542d4e6
jun.hu <mindjun.hu@gmail.com>
2020-06-14 13:46:25 +0800

    add leetcode 739 func

--- pratice_script.py
+++ pratice_script.py
@@ -638,23 +638,3 @@ def count(n, x):
 
 
 print(count(2593, 5))
-
-# leetcode 739
-# https://leetcode-cn.com/problems/daily-temperatures/
-# 使用单点递增栈
-class Solution:
-    def dailyTemperatures(self, T: List[int]) -> List[int]:
-        res = [0 for _ in range(len(T))]
-        stack = list()
-
-        for i in range(len(T))[::-1]:
-            while stack and T[i] >= T[stack[-1]]:
-                stack.pop()
-            res[i] = stack[-1] - i if stack else 0
-            stack.append(i)
-        return res
-
-
-temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-# [1, 1, 4, 2, 1, 1, 0, 0]
-print(Solution().dailyTemperatures(temperatures))
