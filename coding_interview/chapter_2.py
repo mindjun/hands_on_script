39051758b06748a36d26a71e74fc8162ea89eb50
jun.hu <mindjun.hu@gmail.com>
2020-06-27 20:10:31 +0800

    add a recursive func

--- coding_interview/chapter_2.py
+++ coding_interview/chapter_2.py
@@ -54,3 +54,42 @@ yang_matrix = [[1, 4, 7, 11, 15],
                [10, 13, 14, 17, 24],
                [20, 21, 23, 26, 30]]
 print(find_num_in_matrix(yang_matrix, 31))
+
+
+# 回溯法，查找路径
+def find_path(matrix, target_str):
+    """
+    查找 matrix 中是否存在 target_str 的路径
+    :param matrix:
+    :param target_str:
+    :return:
+    """
+    def helper(x, y, index):
+        has_path = False
+        # 当 index 到达最后一位的时候，直接返回
+        if index == len(target_str) - 1 and matrix[x][y] == target_str[index]:
+            return True
+
+        if 0 <= x < size_x and 0 <= y < size_y and matrix[x][y] == target_str[index] and not visited[x][y]:
+            index += 1
+            visited[x][y] = 1
+            has_path = helper(x-1, y, index) or helper(x+1, y, index) or helper(x, y-1, index) or helper(x, y+1, index)
+            if not has_path:
+                index -= 1
+                visited[x][y] = 0
+        return has_path
+
+    size_x, size_y = len(matrix), len(matrix[0])
+    target_index = 0
+    visited = [[0 for _ in range(size_y)] for _ in range(size_x)]
+    for i in range(size_x):
+        for j in range(size_y):
+            if helper(i, j, target_index):
+                return True
+    return False
+
+
+_matrix = [['a', 'b', 't', 'g'],
+           ['c', 'f', 'c', 's'],
+           ['j', 'd', 'e', 'h']]
+print(find_path(_matrix, 'bfcd'))
