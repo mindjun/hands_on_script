3f04850ad04832197b885c3dc5d4749ee6ee959c
hujun <hujun02@imdada.cn>
2020-06-30 19:58:01 +0800

    add an allPossibleFBT func

--- tree_operate/some_leetcode_func.py
+++ tree_operate/some_leetcode_func.py
@@ -4,6 +4,7 @@
 #     traverse(root.left);
 #     traverse(root.right);
 # }
+from typing import List
 
 
 # N 叉树的前序遍历
@@ -451,3 +452,66 @@ def dfs_flip_equiv(root_1: TreeNode, root_2: TreeNode) -> bool:
         return False
 
     return dfs(root_1, root_2)
+
+
+# 所有可能的满二叉树
+def all_possible_fbt(n):
+    res = list()
+    root = TreeNode(0)
+
+    # 要么有两个孩子，要么一个都没有
+    def helper(_root, node, count):
+        if count == n - 1:
+            res.append(_root)
+
+
+class Solution:
+    # 子问题：构造一棵满二叉树
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        res = []
+        if N == 1:
+            return [TreeNode(0)]
+        # 结点个数必须是奇数
+        if N % 2 == 0:
+            return []
+
+        # 左子树分配一个节点
+        left_num = 1
+        # 右子树可以分配到 N - 1 - 1 = N - 2 个节点
+        right_num = N - 2
+
+        while right_num > 0:
+            # 递归构造左子树
+            left_tree = self.allPossibleFBT(left_num)
+            # 递归构造右子树
+            right_tree = self.allPossibleFBT(right_num)
+            # 具体构造过程
+            for i in range(len(left_tree)):
+                for j in range(len(right_tree)):
+                    root = TreeNode(0)
+                    root.left = left_tree[i]
+                    root.right = right_tree[j]
+                    res.append(root)
+            left_num += 2
+            right_num -= 2
+
+        return res
+
+
+# class Solution(object):
+#     memo = {0: [], 1: [TreeNode(0)]}
+#
+#     def allPossibleFBT(self, N):
+#         if N not in Solution.memo:
+#             ans = []
+#             for x in range(N):
+#                 y = N - 1 - x
+#                 for left in self.allPossibleFBT(x):
+#                     for right in self.allPossibleFBT(y):
+#                         bns = TreeNode(0)
+#                         bns.left = left
+#                         bns.right = right
+#                         ans.append(bns)
+#             Solution.memo[N] = ans
+#
+#         return Solution.memo[N]
