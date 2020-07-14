f8cd4a2f8e26bba9eb258bed0d6b99857241ad26
jun.hu <mindjun.hu@gmail.com>
2020-07-14 22:10:00 +0800

    some dp func

--- dp/some_dp_func.py
+++ dp/some_dp_func.py
@@ -180,11 +180,11 @@ def unique_paths_dfs(m, n):
             return
 
         path.append((x, y))
-        dfs(x+1, y, path)
+        dfs(x + 1, y, path)
         path.pop()
 
         path.append((x, y))
-        dfs(x, y+1, path)
+        dfs(x, y + 1, path)
         path.pop()
 
     dfs(0, 0, [])
@@ -192,10 +192,103 @@ def unique_paths_dfs(m, n):
         path_str = ''
         for sub_path in _path:
             path_str += f'{sub_path} ==> '
-        path_str = path_str + f'{(m-1, n-1)}'
+        path_str = path_str + f'{(m - 1, n - 1)}'
         print(path_str)
     return path_count
 
 
 print(unique_paths_dfs(3, 7))
 
+
+def unique_paths_dp(m, n):
+    dp = [[1] * m for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if i == 0 and j == 0:
+                continue
+            if j == 0:
+                dp[i][j] = dp[i - 1][j]
+                continue
+            if i == 0:
+                dp[i][j] = dp[i][j - 1]
+                continue
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[-1][-1]
+
+
+print(unique_paths_dp(3, 7))
+
+
+# 这里涉及到滚动数组的概念
+# 可以理解为每次求解一行的解，每一列的值只与前一行相关
+# 下一列的值也只与前一行和前一列相关，而前一行的值已经暂存在数组中，所以只需要取前一列的值
+def unique_paths_dp_(m, n):
+    if n < m:
+        n, m = m, n
+    dp = [1] * m
+    for i in range(1, n):
+        for j in range(1, m):
+            dp[j] += dp[j - 1]
+    return dp[-1]
+
+
+print(unique_paths_dp_(3, 7))
+
+
+# https://leetcode-cn.com/problems/unique-paths-ii/
+def unique_paths_dp_ii(matrix_):
+    if not matrix_:
+        return 0
+
+    n, m = len(matrix_), len(matrix_[0])
+    dp = [0] * m
+    if matrix_[0][0] == 0:
+        dp[0] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if matrix_[i][j] == 1:
+                dp[j] = 0
+                continue
+            if j - 1 > 0 and matrix_[i][j] == 0:
+                dp[j] += dp[j - 1]
+    return dp[-1]
+
+
+# https://leetcode-cn.com/problems/jump-game/
+def can_jump(nums):
+    farthest, size = 0, len(nums)
+    for i in range(size):
+        farthest = max(farthest, i + nums[i])
+        if farthest >= size - 1:
+            return True
+        if farthest <= i:
+            return False
+    return farthest >= size - 1
+
+
+# tail to head
+def can_jump_dp(nums):
+    # 从后往前依次判断，当前的 left + i 是否能
+    left = len(nums) - 1
+    for i in range(len(nums) - 2, -1, -1):
+        left = i if i + left >= left else left
+    return left == 0
+
+
+print(can_jump([3, 2, 1, 0, 4]))
+
+
+# https://leetcode-cn.com/problems/jump-game-ii/
+def jump(nums):
+    dp = [1] * len(nums)
+    dp[0] = 0
+    for i in range(1, len(nums)):
+        temp_list = [dp[i - j] for j in range(nums[i]) if i - j >= 0]
+        print(f'now temp_list is {temp_list}, now i is {i}')
+        dp[i] = min(temp_list) + 1
+    return dp[-1]
+
+
+print(jump([2, 3, 1, 1, 4]))
