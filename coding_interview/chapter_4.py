41c72ac92cbf90753f2da485bf25a441edf2bb0a
jun.hu <mindjun.hu@gmail.com>
2020-07-25 17:52:11 +0800

    add some func

--- coding_interview/chapter_4.py
+++ coding_interview/chapter_4.py
@@ -164,3 +164,138 @@ def level_order(root: TreeNode) -> List[List[int]]:
 
 t = TreeNode(1, TreeNode(2, left=TreeNode(4)), TreeNode(3, right=TreeNode(5)))
 print(level_order(t))
+
+
+# https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/
+# 后序遍历，根节点一定是最后一个元素，使用该元素将剩下的元素分为左子树和右子树，找到分隔的下标，如果右子树中出现小于根节点值的元素
+# 直接返回 False，该序列一定不是后序遍历的序列
+def verify_post_order(nums):
+    def verify_helper(_nums):
+        print(_nums)
+        if not _nums:
+            return False
+
+        root_num = _nums.pop()
+
+        left_index, left_nums = 0, list()
+        for i in range(len(_nums)):
+            if _nums[i] > root_num:
+                break
+            left_index += 1
+            left_nums.append(_nums[i])
+
+        for j in range(left_index, len(_nums)):
+            if _nums[j] < root_num:
+                return False
+
+        left = True
+        if left_index > 0:
+            left = verify_helper(left_nums)
+
+        right, right_nums = True, _nums[left_index:]
+        if left_index < len(_nums) - 1:
+            right = verify_helper(right_nums)
+        return left and right
+
+    if not nums:
+        return True
+    return verify_helper(nums)
+
+
+print(verify_post_order([5, 7, 6, 9, 11, 10, 8]))
+
+
+# https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/solution/mian-shi-ti-33-er-cha-sou-suo-shu-de-hou-xu-bian-6/
+# 使用单调栈进行判断
+def verify_post_order_ii(nums):
+    stack, root = list(), float('inf')
+    # 后序遍历的倒序列表，[根节点|右子树|左子树]
+    for num in nums[::-1]:
+        if num > root:
+            return False
+        # 在 stack 找到 num 的父节点
+        while stack and stack[-1] > num:
+            root = stack.pop()
+        stack.append(num)
+    return True
+
+
+# https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/submissions/
+# 回溯法进行计算，类似于决策树
+class Solution:
+    def __init__(self):
+        self.sum_func = sum
+
+    def pathSum(self, root: TreeNode, sum_: int) -> List[List[int]]:
+        result = list()
+
+        def back_track(node, path):
+            if not node.left and not node.right:
+                if self.sum_func(path) == sum_:
+                    result.append(path.copy())
+                return
+            if node.left:
+                path.append(node.left.val)
+                back_track(node.left, path)
+                path.pop()
+            if node.right:
+                path.append(node.right.val)
+                back_track(node.right, path)
+                path.pop()
+            return
+
+        if not root:
+            return []
+
+        back_track(root, [root.val])
+        return result
+
+
+# https://leetcode-cn.com/problems/path-sum-ii/
+# dfs 每次带上 path 和当前 sum
+def path_sum_ii(root, target):
+    result = list()
+
+    def dfs(node, path, _sum):
+        if not node:
+            return
+        if not node.left and not node.right and node.val - _sum == 0:
+            result.append(path + [node.val])
+            return
+        dfs(node.left, path+[node.val], _sum-node.val)
+        dfs(node.right, path+[node.val], _sum-node.val)
+        return
+    dfs(root, [], target)
+    return result
+
+
+_r = TreeNode(5, TreeNode(4, left=TreeNode(11, TreeNode(7), TreeNode(2))),
+              TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1))))
+print(Solution().pathSum(_r, 22))
+print(path_sum_ii(_r, 22))
+
+
+# https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/solution/mian-shi-ti-36-er-cha-sou-suo-shu-yu-shuang-xian-5/
+def bst_to_double_linked(root):
+    if not root:
+        return None
+    _head, pre = None, None
+
+    def dfs(cur):
+        if not cur:
+            return
+        dfs(cur.left)
+        nonlocal pre
+        nonlocal _head
+        if pre:
+            pre.right, cur.left = cur, pre
+        else:
+            _head = cur
+        # 不断替换 pre 为 cur，中序遍历，第一次 return 的时候，pre 为 None， cur 为最左边的叶节点
+        # 此时将 pre 设置为 cur，即最左边的叶节点，下一个即将访问的节点就是该节点的父节点
+        pre = cur
+        dfs(cur.right)
+
+    dfs(root)
+    _head.left, pre.right = pre, _head
+    return _head
