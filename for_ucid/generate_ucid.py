c70334f2a0335586911ae6ff58d91906cf0f2c95
jun.hu <mindjun.hu@gmail.com>
2022-06-13 22:30:40 +0800

    optimise

--- for_ucid/generate_ucid.py
+++ for_ucid/generate_ucid.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 
 # data_source
@@ -52,78 +53,124 @@ class UCIDGenerator(object):
         self.excel_data['UCID'] = None
         self.excel_data = self.excel_data.fillna('')
         self.excel_data['member_type'] = self.excel_data['member_type'].map(lambda x: '个人' if x.startswith('个人') else '企业')
+        start = int(time.time())
+        print('group_by start at: {}'.format(start))
         groups = self.excel_data.groupby(by='member_type')
+        print('group_by cost: {}'.format(int(time.time()) - start))
+        # 企业
         company_df = groups.get_group('企业')
+        self.company_group(company_df)
+        # 个人
+        person_df = groups.get_group('个人')
+        self.person_group(person_df)
 
+    def person_group(self, person_df):
+        start = int(time.time())
+        print('person sorting start at: {}'.format(start))
+        person_df.sort_values('legal_cert_no_mask', inplace=True)
+        print('person sorting cost: {}'.format(int(time.time()) - start))
+
+        legal_cert_no_mask_set, candidate_idx, index = set(), list(), -1
+        last_prefix = person_df['legal_cert_no_mask'][0]
+        for name, row in person_df['legal_cert_no_mask'].iteritems():
+            index += 1
+            if index == 0:
+                self.update_person_container(legal_cert_no_mask_set, candidate_idx, name)
+                continue
+            if row == last_prefix:
+                self.update_person_container(legal_cert_no_mask_set, candidate_idx, name)
+            else:
+                self.person_group_ucid(legal_cert_no_mask_set, candidate_idx)
+                last_prefix = person_df['legal_cert_no_mask'][index]
+                self.update_person_container(legal_cert_no_mask_set, candidate_idx, name)
+            # 最后一组
+            if index == len(person_df) - 1:
+                self.person_group_ucid(legal_cert_no_mask_set, candidate_idx)
+
+    def update_person_container(self, legal_cert_no_mask_set, candidate_idx, name):
+        legal_cert_no_mask_set.add(self.excel_data.iloc[name]['legal_cert_no_mask'])
+        candidate_idx.append(name)
+
+    def person_group_ucid(self, legal_cert_no_mask_set, candidate_idx):
+        if len(legal_cert_no_mask_set) > 1:
+            self.set_conflict_ucid_by_candidate_idx(candidate_idx, '个人')
+        elif '' in legal_cert_no_mask_set:
+            self.excel_data.loc[candidate_idx, 'UCID'] = self.get_id('个人', False)
+        else:
+            self.excel_data.loc[candidate_idx, 'UCID'] = self.get_id('个人', True)
+        legal_cert_no_mask_set.clear()
+        candidate_idx.clear()
+
+    def company_group(self, company_df):
         company_df['unified_code'] = company_df['unified_code'].astype('str')
         company_df['license_code'] = company_df['license_code'].astype('str')
         company_df['tax_code'] = company_df['tax_code'].astype('str')
         company_df['contact'] = company_df['unified_code'].str.cat([company_df['license_code'],
                                                                     company_df['tax_code']], sep='_')
+        start = int(time.time())
+        print('company sorting start at: {}'.format(start))
         company_df.sort_values('contact', inplace=True)
+        print('company sorting cost: {}'.format(int(time.time()) - start))
         candidate_idx, license_code_set, tax_code_set = list(), set(), set()
-        temp_unified_code, only_tax_code_prefix = None, None
+        last_prefix = self.set_last_prefix(company_df, 0)
         index = -1
 
         for name, row in company_df['contact'].iteritems():
             index += 1
-            # 全部为空的情况
-            if row == '___':
+            # 信用代码，营业执照，税号全部为空的情况
+            if row == '__':
+                if len(license_code_set) != 0:
+                    self.fill_ucid(last_prefix, license_code_set, tax_code_set, candidate_idx)
                 self.excel_data.loc[name, "UCID"] = self.get_id('企业', False)
+                last_prefix = '___'
                 continue
 
-            # 前两个为空的情况
-            if row.startswith('__'):
-                only_tax_code_prefix = '__'
-                tax_code_set.add(company_df.iloc[index]['tax_code'])
-                candidate_idx.append(name)
-                continue
-            if only_tax_code_prefix == '__':
-                self.handle_only_tax_code(tax_code_set, candidate_idx)
-                only_tax_code_prefix = None
-
-            if temp_unified_code is None:
-                temp_unified_code = company_df.iloc[index]['unified_code']
-                self.update_container(name, license_code_set, tax_code_set, candidate_idx)
+            if index == 0:
+                self.update_container(name, license_code_set, tax_code_set, candidate_idx, last_prefix)
                 continue
 
-            if company_df.iloc[index]['unified_code'] == temp_unified_code:
-                self.update_container(name, license_code_set, tax_code_set, candidate_idx)
-                continue
-            # unified_code 不一样，说明已经进入下一个分组，进行判断以及清理
-            self.handle_full_condition(license_code_set, tax_code_set, candidate_idx)
-            temp_unified_code = company_df.iloc[index]['unified_code']
-            self.update_container(name, license_code_set, tax_code_set, candidate_idx)
+            if row.startswith(last_prefix):
+                self.update_container(name, license_code_set, tax_code_set, candidate_idx, last_prefix)
+            else:
+                self.fill_ucid(last_prefix, license_code_set, tax_code_set, candidate_idx)
+                last_prefix = self.set_last_prefix(company_df, index)
+                self.update_container(name, license_code_set, tax_code_set, candidate_idx, last_prefix)
 
-        person_df = groups.get_group('个人')
-        person_df.sort_values('legal_cert_no_mask', inplace=True)
-        legal_cert_no_mask, candidate_idx, person_index = None, list(), -1
-        for name, row in person_df['legal_cert_no_mask'].iteritems():
-            person_index += 1
-            if legal_cert_no_mask is None:
-                legal_cert_no_mask = person_df.iloc[person_index]['legal_cert_no_mask']
-                candidate_idx.append(name)
-                continue
-            if person_df.iloc[person_index]['legal_cert_no_mask'] == legal_cert_no_mask:
-                candidate_idx.append(name)
-                continue
-            self.excel_data.loc[candidate_idx, 'UCID'] = self.get_id('个人', True)
-            candidate_idx.clear()
+            # 最后一组数据
+            if index == len(company_df) - 1:
+                self.fill_ucid(last_prefix, license_code_set, tax_code_set, candidate_idx)
 
-    def update_container(self, name, license_code_set, tax_code_set, candidate_idx):
-        license_code_set.add(self.excel_data.iloc[name]['license_code'])
+    def update_container(self, name, license_code_set, tax_code_set, candidate_idx, last_prefix):
         tax_code_set.add(self.excel_data.iloc[name]['tax_code'])
+        if last_prefix != '__':
+            license_code_set.add(self.excel_data.iloc[name]['license_code'])
         candidate_idx.append(name)
 
+    @staticmethod
+    def set_last_prefix(company_df, index):
+        if company_df.iloc[index]['contact'].startswith('__'):
+            last_prefix = '__'
+        else:
+            last_prefix = '{}_'.format(company_df.iloc[index]['unified_code'])
+        return last_prefix
+
+    def fill_ucid(self, last_prefix, license_code_set, tax_code_set, candidate_idx):
+        if last_prefix == '__':
+            self.handle_only_tax_code(tax_code_set, candidate_idx)
+        else:
+            self.handle_full_condition(license_code_set, tax_code_set, candidate_idx)
+        # 清空
+        candidate_idx.clear()
+        license_code_set.clear()
+        tax_code_set.clear()
+
     # 处理只有税号的情况
+    # todo 只有税号的情况需要特别讨论
     def handle_only_tax_code(self, tax_code_set, candidate_idx):
         if len(tax_code_set) > 2 or (len(tax_code_set) == 2 and '' not in tax_code_set):
             self.set_conflict_ucid_by_candidate_idx(candidate_idx, '企业')
         else:
             self.excel_data.loc[candidate_idx, "UCID"] = self.get_id('企业', True)
-        # 清空
-        candidate_idx.clear()
-        tax_code_set.clear()
 
     # 多个冲突的 ucid，需要一个一个设置
     def set_conflict_ucid_by_candidate_idx(self, candidate_idx, member_type):
@@ -133,14 +180,11 @@ class UCIDGenerator(object):
     def handle_full_condition(self, license_code_set, tax_code_set, candidate_idx):
         if len(license_code_set) > 2 or (len(license_code_set) == 2 and '' not in license_code_set):
             self.set_conflict_ucid_by_candidate_idx(candidate_idx, '企业')
-        elif len(tax_code_set) > 2 and (len(tax_code_set) == 2 and '' not in tax_code_set):
+        elif len(tax_code_set) > 2 or (len(tax_code_set) == 2 and '' not in tax_code_set):
             self.set_conflict_ucid_by_candidate_idx(candidate_idx, '企业')
         else:
             # 不冲突
             self.excel_data.loc[candidate_idx, 'UCID'] = self.get_id('企业', True)
-        candidate_idx.clear()
-        license_code_set.clear()
-        tax_code_set.clear()
 
     def generate_ucid(self):
         self.data_from_xlsx()
