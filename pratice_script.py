9bcac3bb7c94f6be9c837003c5ffcc5d9dd451d3
jun.hu <mindjun.hu@gmail.com>
2022-09-06 10:39:46 +0800

    add a coin change function

--- pratice_script.py
+++ pratice_script.py
@@ -459,3 +459,17 @@ def str_str(haystack: str, needle: str):
 
 
 print(str_str('hello', 'll'))
+
+
+def coin_change(coins, amount):
+    dp = [amount + 1] * (amount + 1)
+    dp[0] = 0
+    for n in range(amount + 1):
+        for coin in coins:
+            if n - coin < 0:
+                continue
+            dp[n] = min(dp[n], 1 + dp[n-coin])
+    return dp
+
+
+print(coin_change([1, 2, 5], 11))
